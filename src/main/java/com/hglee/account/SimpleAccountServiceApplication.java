5f73f654e2377e8fa1a7af8b07e22a216d861be3
coderhglee <goodna17@gmail.com>
2023-01-06 18:02:18 +0900

    setup: init spring webmvc project

---
+++ src/main/java/com/hglee/account/SimpleAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hglee.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAccountServiceApplication.class, args);
+	}
+
+}
