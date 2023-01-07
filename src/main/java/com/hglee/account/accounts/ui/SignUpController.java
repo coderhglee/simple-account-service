66d1b975ac0c29f99a34fece20d216d1b080442b
coderhglee <goodna17@gmail.com>
2023-01-07 19:40:18 +0900

    feat: 인증코드 verify endpoint 추가
    - 전화번호로 발급된 인증번호를 verify 하는 endpoint 추가
    
    refs #1

--- src/main/java/com/hglee/account/accounts/ui/SignUpController.java
+++ src/main/java/com/hglee/account/accounts/ui/SignUpController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hglee.account.accounts.application.command.RequestAccountVerificationCommand;
+import com.hglee.account.accounts.application.command.VerifyAccountCommand;
 import com.hglee.account.accounts.application.usecase.RequestAccountVerificationUseCase;
+import com.hglee.account.accounts.application.usecase.VerifyAccountUseCase;
 import com.hglee.account.accounts.dto.RequestAccountVerificationMobileRequest;
+import com.hglee.account.accounts.dto.VerifyMobileRequest;
 
 @RestController
 public class SignUpController {
 
 	private final RequestAccountVerificationUseCase requestAccountVerificationUseCase;
+	private final VerifyAccountUseCase verifyAccountUseCase;
 
-	public SignUpController(RequestAccountVerificationUseCase requestAccountVerificationUseCase) {
+	public SignUpController(RequestAccountVerificationUseCase requestAccountVerificationUseCase,
+			VerifyAccountUseCase verifyAccountUseCase) {
 		this.requestAccountVerificationUseCase = requestAccountVerificationUseCase;
+		this.verifyAccountUseCase = verifyAccountUseCase;
 	}
 
 	@PostMapping(value = "/sign-up/request-account-verification-mobile", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,4 +33,12 @@ public class SignUpController {
 
 		return ResponseEntity.ok().build();
 	}
+
+	@PostMapping(value = "/sign-up/verify-mobile", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> verifyMobile(@RequestBody final VerifyMobileRequest request) {
+		verifyAccountUseCase.execute(new VerifyAccountCommand(request.getMobile(), request.getCode()));
+
+		return ResponseEntity.ok().build();
+	}
+
 }
--- src/test/java/com/hglee/account/accounts/ui/SignUpControllerTest.java
+++ src/test/java/com/hglee/account/accounts/ui/SignUpControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +17,8 @@ import org.springframework.http.MediaType;
 
 import com.hglee.account.AcceptanceTest;
 import com.hglee.account.accounts.domain.Account;
+import com.hglee.account.accounts.domain.PinCode;
+import com.hglee.account.accounts.domain.Status;
 import com.hglee.account.accounts.factory.AccountFactory;
 import com.hglee.account.accounts.persistence.repository.AccountRepository;
 
@@ -104,6 +107,178 @@ class SignUpControllerTest extends AcceptanceTest {
 						.assertThat(status().is4xxClientError());
 			}
 		}
+	}
+
+	@Nested
+	@DisplayName("Describe: POST /sign-up/verify-mobile")
+	class post_verify_mobile {
+		@Nested
+		@DisplayName("Context: 인증코드가 발급되고 유효한 인증코드인 경우")
+		class not_expires_pin_code {
+			String mobile;
+			String code;
+
+			@BeforeEach
+			void before() {
+				AccountFactory accountFactory = AccountFactory.build();
+
+				mobile = accountFactory.getMobile();
+
+				전화번호_인증_요청(mobile);
+				Account account = accountRepository.findByMobile(mobile).get();
+				code = account.getPinCode().getCode();
+			}
+
+			@DisplayName("It: 코드를 인증할 수 있다.")
+			@Test
+			void success_request_verification() {
+				인증코드_인증_요청(mobile, code);
+			}
+		}
+
+		@Nested
+		@DisplayName("Context: 만료된 인증코드인 경우")
+		class expires_pin_code {
+			String mobile;
+			String code;
+
+			@BeforeEach
+			void before() {
+				AccountFactory accountFactory = AccountFactory.build();
+				code = accountFactory.getPinCode().getCode();
+				mobile = accountFactory.getMobile();
+
+				Account account = new Account(accountFactory.getId(), mobile, accountFactory.getEmail(),
+						Status.VERIFICATION_REQUESTED,
+						accountFactory.getName(), accountFactory.getNickName(),
+						new PinCode(code, LocalDateTime.now().minusMinutes(1), LocalDateTime.now()));
+
+				accountRepository.save(account);
+			}
+
+			@DisplayName("It: 403 에러가 발생한다.")
+			@Test
+			void failed_request_verification() {
+				Map<String, String> params = new HashMap<>();
+				params.put("mobile", mobile);
+				params.put("code", code);
+
+				given().body(params)
+						.contentType(MediaType.APPLICATION_JSON_VALUE)
+						.when()
+						.post("/sign-up/verify-mobile")
+						.then()
+						.apply(print())
+						.assertThat(status().is4xxClientError());
+			}
+		}
+
+		@Nested
+		@DisplayName("Context: 사용된 인증코드인 경우")
+		class used_pin_code {
+			String mobile;
+
+			@BeforeEach
+			void before() {
+				AccountFactory accountFactory = AccountFactory.build();
+
+				mobile = accountFactory.getMobile();
+
+				전화번호_인증_요청(mobile);
+			}
+
+			@DisplayName("It: 403 에러가 발생한다.")
+			@Test
+			void failed_request_verification() {
+				Map<String, String> params = new HashMap<>();
+				params.put("mobile", mobile);
+				params.put("code", "123456");
+
+				given().body(params)
+						.contentType(MediaType.APPLICATION_JSON_VALUE)
+						.when()
+						.post("/sign-up/verify-mobile")
+						.then()
+						.apply(print())
+						.assertThat(status().is4xxClientError());
+			}
+		}
+
+		@Nested
+		@DisplayName("Context: 일치하지 않는 인증코드인 경우")
+		class unmatched_pin_code {
+			String mobile;
+
+			@BeforeEach
+			void before() {
+				AccountFactory accountFactory = AccountFactory.build();
+
+				mobile = accountFactory.getMobile();
+
+				전화번호_인증_요청(mobile);
+			}
+
+			@DisplayName("It: 403 에러가 발생한다.")
+			@Test
+			void failed_request_verification() {
+				Map<String, String> params = new HashMap<>();
+				params.put("mobile", mobile);
+				params.put("code", "123456");
+
+				given().body(params)
+						.contentType(MediaType.APPLICATION_JSON_VALUE)
+						.when()
+						.post("/sign-up/verify-mobile")
+						.then()
+						.apply(print())
+						.assertThat(status().is4xxClientError());
+			}
+		}
+
+		@Nested
+		@DisplayName("Context: 이미 가입된 전화번호의 인증코드인 경우")
+		class already_sign_up_account {
+			String mobile;
+			String code;
+
+			@BeforeEach
+			void before() {
+				Account signedUpAccount = AccountFactory.isSignedUpAccount();
+
+				mobile = signedUpAccount.getMobile();
+				code = signedUpAccount.getPinCode().getCode();
+			}
+
+			@DisplayName("It: 403 에러가 발생한다.")
+			@Test
+			void failed_request_verification() {
+				Map<String, String> params = new HashMap<>();
+				params.put("mobile", mobile);
+				params.put("code", code);
+
+				given().body(params)
+						.contentType(MediaType.APPLICATION_JSON_VALUE)
+						.when()
+						.post("/sign-up/verify-mobile")
+						.then()
+						.apply(print())
+						.assertThat(status().is4xxClientError());
+			}
+		}
+	}
+
+	private void 인증코드_인증_요청(String mobile, String code) {
+		Map<String, String> params = new HashMap<>();
+		params.put("mobile", mobile);
+		params.put("code", code);
+
+		given().body(params)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.when()
+				.post("/sign-up/verify-mobile")
+				.then()
+				.apply(print())
+				.assertThat(status().isOk());
 	}
 
 	private void 전화번호_인증_요청(String mobile) {
